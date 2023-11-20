79cd31f9b9cddd113135086bdc503502dae7d9a0
smsmpanda <64564713+smsmpanda@users.noreply.github.com>
2023-11-20 23:34:00 +0800

    Binding各种用法

---
+++ src/KungfuWpfExamples/Binding.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KungfuWpfExamples
+{
+    /// <summary>
+    /// Binding.xaml 的交互逻辑
+    /// </summary>
+    public partial class Binding : Window
+    {
+        public Binding()
+        {
+            InitializeComponent();
+
+            //DataContext = new BindingViewModel();
+        }
+    }
+
+
+    public class BindingViewModel 
+    {
+        public string Message { get; set; }
+        public override string ToString()
+        {
+            return "Empty";
+        }
+    }
+
+    public class MyResources
+    {
+        public string Message => "Hello Resource";
+        public static string StaticString = "Static String";
+    }
+}
