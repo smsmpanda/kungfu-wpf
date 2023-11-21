58a2d20257c7d26553e64d0f7d6e69ffcd00f56b
smsmpanda <64564713+smsmpanda@users.noreply.github.com>
2023-11-21 22:59:48 +0800

    ElementName & RelativeSource的一些局限新及解决方式

---
+++ src/KungfuWpfExamples/BindingProxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KungfuWpfExamples
+{
+    class BindingProxy : Freezable
+    {
+        protected override Freezable CreateInstanceCore() => new BindingProxy();
+
+        public object Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register(
+            nameof(Data),
+            typeof(object),
+            typeof(BindingProxy)
+        );
+    }
+}
---
+++ src/KungfuWpfExamples/RelativeSourceElementName.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KungfuWpfExamples
+{
+    /// <summary>
+    /// RelativeSourceElementName.xaml 的交互逻辑
+    /// </summary>
+    public partial class RelativeSourceElementName : Window
+    {
+        public RelativeSourceElementName()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Onload(object sender, RoutedEventArgs e)
+        {
+            var txt = new TextBox 
+            {
+                Name="txt"
+            };
+            this.RegisterName("txt",txt);
+            panel.Children.Add(txt);
+        }
+    }
+}
