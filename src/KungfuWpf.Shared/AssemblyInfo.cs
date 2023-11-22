4b537dbefc7de8c7e52e83d40295000fa514bdb5
smsmpanda <64564713+smsmpanda@users.noreply.github.com>
2023-11-22 21:54:57 +0800

    xmlns的作用及制作方法

---
+++ src/KungfuWpf.Shared/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://github.com/smsmpand/Wpf", "KungfuWpf.Shared.Utils")]
+[assembly: XmlnsDefinition("http://github.com/smsmpand/Wpf", "KungfuWpf.Shared.Converts")]
+[assembly: XmlnsDefinition("http://github.com/smsmpand/Wpf", "KungfuWpf.Shared.CustomControls")]
+[assembly: XmlnsDefinition("http://github.com/smsmpand/Wpf", "KungfuWpf.Shared.Behaviors")]
+[assembly:XmlnsPrefix("http://smsmpanda/wpf","g")]
+
+
