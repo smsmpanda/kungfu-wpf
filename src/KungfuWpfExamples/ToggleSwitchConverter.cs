51ab6d0843e448b4d74e6f188915202907fe9ad5
jiangr <jiangrui@rhtect.com>
2023-12-26 13:53:53 +0800

    自定义开关按钮

---
+++ src/KungfuWpfExamples/ToggleSwitchConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KungfuWpfExamples
+{
+    public class ToggleSwitchConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double height)
+            {
+                var property = parameter as string;
+                switch (property)
+                {
+                    case "ButtonSize":
+                        return (height - 8);
+                    case "OutsideCorner":
+                        return new CornerRadius(height / 2);
+                    case "InteriorCorner":
+                        return new CornerRadius((height - 8) / 2);
+                }
+            }
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/KungfuWpfExamples/WPF2D.xaml.cs
+++ src/KungfuWpfExamples/WPF2D.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,51 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace KungfuWpfExamples
 {
     /// <summary>
     /// WPF2D.xaml 的交互逻辑
     /// </summary>
-    public partial class WPF2D : Window
+    public partial class WPF2D : Window, INotifyPropertyChanged
     {
+        private const double AnglePerHour = 30.0;
+        private const double AnglePerMinute = 6.0;
+        private const double AnglePerSecond = 6.0;
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public double HourAngle { get; private set; }
+        public double MinuteAngle { get; private set; }
+        public double SecondAngle { get; private set; }
         public WPF2D()
         {
             InitializeComponent();
+
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            DataContext = this;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+
+
+            HourAngle = currentTime.Hour * AnglePerHour + currentTime.Minute * AnglePerMinute / 12;
+            MinuteAngle = currentTime.Minute * AnglePerMinute + currentTime.Second * AnglePerSecond / 60;
+            SecondAngle = currentTime.Second * AnglePerSecond;
+
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HourAngle)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MinuteAngle)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SecondAngle)));
         }
     }
 }
