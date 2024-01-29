bc8bd4612a93f20d1b6fc0f4a88da97c430163c7
smsmpanda <64564713+smsmpanda@users.noreply.github.com>
2024-01-30 00:17:32 +0800

    custome window title area

---
+++ src/KungfuWpfExamples/CustomeWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KungfuWpfExamples
+{
+    /// <summary>
+    /// CustomeWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class CustomeWindow : Window
+    {
+        public CustomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                this.btnMaximize.Content = "&#xe600;";
+            }
+            else {
+                this.WindowState = WindowState.Maximized;
+                this.btnMaximize.Content = "&#xe601;";
+            }
+        }
+    }
+}
